d4d4b260b190f23d0a2dd1f921d432fc250b63fe
L IN <linzb93@qq.com>
2019-12-26 16:34:33 +0800

    新增跨域

--- app/index.js
+++ app/index.js
@@ -10,6 +10,14 @@ function haltOnTimedout (req, res, next) {
     next();
   }
 }
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
 app.set('views', './app/view');
 app.set('view engine', 'ejs');
 // 超时处理
